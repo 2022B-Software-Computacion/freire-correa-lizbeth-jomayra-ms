78e276f2fa66fc15af41f1e8fc7cf43ef7274548
Lizbeth Freire <lizbeth.freire@epn.edu.ec>
2023-03-19 11:23:08 -0500

    Universidad Adapter
    Creación de dos clases

---
+++ UniversidadFacultades/app/src/main/java/com/example/universidadfacultades/adapter/UniversidadViewHolder.kt
@@ -0,0 +1,8 @@
+package com.example.universidadfacultades.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class UniversidadViewHolder (view: View): RecyclerView.ViewHolder(view){
+
+}
